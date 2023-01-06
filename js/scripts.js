50903ef6d1cbcaa6dbc781204540c1c47d0e42b4
Eduardo Ramalho <2220854@my.ipleiria.pt>
2023-01-06 16:41:00 +0000

    java script

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,18 @@ let weather ={
             + this.apiKey
         )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then(data => {
+        const { name } = data.name;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".cidade").innerHTML = "Weather in " + name;
+        document.querySelector(".icon").src = 
+            "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            document.querySelector(".descricao").innerHTML = description;
+            document.querySelector(".temperatura").innerHTML = temp + "ºC";
+        }
+        );
     },
-    displayWeather: function (data) {
-        
-    }
 };

4729c436a7659c162e4198f3511c0f21d7ba33c4
Eduardo Ramalho <2220854@my.ipleiria.pt>
2023-01-07 16:21:25 +0000

    trabalho no fim de semana

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,28 @@ let weather ={
         )
         .then((response) => response.json())
         .then(data => {
-        const { name } = data.name;
+        const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
-        document.querySelector(".cidade").innerHTML = "Weather in " + name;
+        const { temp, humidity, pressure } = data.main;
+        const { lon, lat } = data.coord;
+        console.log(name, icon, description, temp, humidity, pressure, lat, lon);
+        document.querySelector(".cidade").innerHTML = "Meteorologia em " + name;
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             document.querySelector(".descricao").innerHTML = description;
             document.querySelector(".temperatura").innerHTML = temp + "ºC";
             document.querySelector(".humidade").innerHTML = "Humidade: " + humidity + "%";
-            document.querySelector(".vento").innerHTML = "Vento: " + speed + "Km/h";
+            document.querySelector(".pressao").innerHTML = "Pressao atmosférica: " + pressure + "hPa";
+            document.querySelector(".coordenadas").innerHTML = "Coordenadas: lon " + lon + " lat " + lat;
         }
         );
     },
+    search: function () {
+        this.fetchWeather(document.querySelector(".procurar").value);
+    }
 };
+
+document
+    .querySelector(".botaoP")
+    .addEventListener("click", function(){
+        weather.search();
+});
